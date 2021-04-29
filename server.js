bdad76dfcecb39143b2ad5ccc4226c0919460069
Omnimble <omnimble@gmail.com>
2021-04-29 15:47:31 +0300

    switched to port 80

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-import {ApolloServer} from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import schemas from './schemas/index.js';
 import resolvers from './resolvers/index.js';
 import express from 'express';
 import connectMongo from './db.js';
 import 'dotenv/config.js';
 
-(async () => {
-   try {
-    await connectMongo();
+(async() => {
+    try {
+        await connectMongo();
 
-    const server = new ApolloServer({
-        typeDefs: schemas,
-        resolvers,
-    });
-  
-    const app = express();
+        const server = new ApolloServer({
+            typeDefs: schemas,
+            resolvers,
+        });
 
-    server.applyMiddleware({ app });
+        const app = express();
 
-    app.use(express.static('public'));
-    app.listen({port: 3000}, () =>
-        console.log(
-            `🚀 Server ready at http://localhost:3000${server.graphqlPath}`),
-    );
-   } catch (e) {
-      console.log('server error: ' + e.message);
-   }
+        server.applyMiddleware({ app });
+
+        app.use(express.static('public'));
+        app.listen({ port: 80 }, () =>
+            console.log(
+                `🚀 Server ready at http://localhost:3000${server.graphqlPath}`),
+        );
+    } catch (e) {
+        console.log('server error: ' + e.message);
+    }
 })();
