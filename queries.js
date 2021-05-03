ffc40876a3ece2ded4b43b3493fb75bcbbaa368d
Omnimble <omnimble@gmail.com>
2021-05-04 01:18:24 +0300

    queries

--- queries.js
+++ queries.js
@@ -1,18 +1,18 @@
 import { gql } from 'apollo-server-express';
 
 const getItems = gql `
-    query {
-      items {
-        id
-        name
-        obtained
-        quantity
-        activity {
-          activityName
-        }
-      }
+query {
+  items {
+    id
+    name
+    obtained
+    quantity
+    activity {
+      activityName
     }
-    `;
+  }
+}
+`;
 
 const modifyItem = gql `
 mutation {
@@ -22,35 +22,33 @@ mutation {
     quantity
   }
 }
-  `;
+`;
 const addItem = gql `
 mutation {
   addItem(id:"60883144a2c5304a34351507", name: "Jar of Miasma", activity: "607410f6b231f4f0d9fa7f44", obtained:true, quantity:5) {
-   id
-   name
-   obtained
-   quantity
-   activity {
-     activityName
-   }
- }
+    id
+    name
+    obtained
+    quantity
+    activity {
+      activityName
+    }
+  }
 }
-  `;
+`;
 
 const deleteItem = gql `
-  mutation {
-    deleteItem(id:"6090175b03fbf43814a51804") {
-     id
-     name
-     obtained
-     quantity
-     activity {
-       activityName
-     }
-   }
- }
-    `;
+mutation {
+  deleteItem(id:"6090175b03fbf43814a51804") {
+    id
+    name
+    obtained
+    quantity
+    activity {
+      activityName
+    }
+  }
+}
+`;
 
-
-
-export default getItemNames;
+export default { getItems, modifyItem, addItem, deleteItem };
